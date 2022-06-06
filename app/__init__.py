6a4d798330ce3afc49a2bc08087f7bc19cc3f9d3
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-06 14:38:19 +0200

    foo

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
@@ -21,6 +22,15 @@ def create_app(env=None):
 
     @app.route("/health")
     def health():
-        return jsonify("healthy")
+        DB_USERNAME = os.environ["DB_USERNAME"]
+        DB_PASSWORD = os.environ["DB_PASSWORD"]
+        DB_HOST = os.environ["DB_HOST"]
+        DB_PORT = os.environ["DB_PORT"]
+        DB_NAME = os.environ["DB_NAME"]
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        )
+
+        return jsonify("healthy" + format(SQLALCHEMY_DATABASE_URI))
 
     return app
