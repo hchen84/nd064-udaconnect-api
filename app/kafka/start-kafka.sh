0f8fc928d3debdf94d1a5b0ba11da293726045fd
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-07 09:17:29 +0200

    add kafka

---
+++ app/kafka/start-kafka.sh
@@ -0,0 +1,10 @@
+# start zookeeper
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
+# start kafka broker
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
+# create topic "location"
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic location --bootstrap-server localhost:9092
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic person --bootstrap-server localhost:9092
+# restart kafka broker
+$KAFKA_HOME/bin/kafka-server-stop.sh
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
