b3018d2c170067d924df7e0c94bcfcb3a2dca13e
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-07 08:16:51 +0200

    update grpc

--- app/grpc/server.py
+++ app/grpc/server.py
@@ -39,19 +39,25 @@ class PersonServicer(service_pb2_grpc.PersonServiceServicer):
         return service_pb2.PersonMessage(**request_value)
 
 
-# Initialize gRPC server
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-service_pb2_grpc.add_LocationServiceServicer_to_server(
-    LocationServicer(), server)
-service_pb2_grpc.add_PersonServiceServicer_to_server(PersonServicer(), server)
+def serve():
+    # Initialize gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    service_pb2_grpc.add_LocationServiceServicer_to_server(
+        LocationServicer(), server)
+    service_pb2_grpc.add_PersonServiceServicer_to_server(
+        PersonServicer(), server)
 
-# TODO: server pod
-print("Server starting on port 5005...")
-server.add_insecure_port("[::]:5005")
-server.start()
-# Keep thread alive
-try:
-    while True:
-        time.sleep(86400)
-except KeyboardInterrupt:
-    server.stop(0)
+    # TODO: server pod
+    print("Server starting on port 5005...")
+    server.add_insecure_port("[::]:5005")
+    server.start()
+    # Keep thread alive
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
