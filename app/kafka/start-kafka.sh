785a110b688c5b946ba15cb3caa62318f05469ae
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-07 16:39:21 +0200

    foo

--- app/kafka/start-kafka.sh
+++ app/kafka/start-kafka.sh
@@ -9,6 +9,8 @@ $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 $KAFKA_HOME/bin/kafka-topics.sh --create --topic location --bootstrap-server localhost:9092
 $KAFKA_HOME/bin/kafka-topics.sh --create --topic person --bootstrap-server localhost:9092
 # restart kafka broker
-$KAFKA_HOME/bin/kafka-server-stop.sh/
+# $KAFKA_HOME/bin/zookeeper-server-stop.sh
+$KAFKA_HOME/bin/kafka-server-stop.sh
+
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 # python3 -u consumer.py
